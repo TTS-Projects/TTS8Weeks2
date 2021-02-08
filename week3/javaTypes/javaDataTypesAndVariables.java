02ad1fd02b8a53efffff97500b10eb42a696a4c1
Miguel <Garzarobm@utexas.edu>
2021-02-08 15:28:29 -0600

    added erroneous code in addition to lab practice

---
+++ week3/javaTypes/javaDataTypesAndVariables.java
@@ -0,0 +1,22 @@
+public class Main {  
+
+ static int methodCount;
+ static String className;
+public static void main(String[] args)   { 
+
+   // TODO Auto-generated method stub   
+ int mainInt = 1;
+    System.out.println("Hello World!");
+    System.out.println("Method count: " + methodCount);
+    System.out.println("Class Name: " + className);
+    methodCount += mainInt;
+    System.out.println("Method count: " + methodCount);
+    float f1 = 856.2f;
+    int castFloat = (int)f1;
+    System.out.println("Float: " + f1 + ", Cast float: " + castFloat); 
+
+	}
+
+
+}
+
---
+++ week3/typesVariablesOperators/myProgram.java
@@ -0,0 +1,29 @@
+public class Main {
+	/* errroneous code
+	public void main(String[] args){
+
+
+	}
+
+
+
+	*/
+
+
+	/* erroneous variables
+
+	public static int int;
+	int hello;
+	System.out.println(hello);
+	String int = 1;
+	System.out.println(int);
+	
+	*/
+
+
+
+
+
+
+
+}
